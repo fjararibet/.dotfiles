5f270536830a685a6c00bd25d9ff0d2e4bf8d2f7
Your Name <fjararibet@gmail.com>
2023-08-20 15:42:52 -0400

    bash and neovim config

---
+++ .config/nvim/lua/colors.lua
@@ -0,0 +1,13 @@
+function ColorVim(color)
+    color = color or "onedark"
+    vim.cmd.colorscheme(color)
+
+    -- if color == 'onedark' then
+    --     return nil
+    -- end
+
+    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+end
+
+ColorVim()
