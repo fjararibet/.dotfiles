83a9959bef8bac7a20ff38648bb3d2e5d78ee224
Felipe Jara <fjararibet@gmail.com>
2024-01-15 22:12:14 -0300

    create installation script

---
+++ .install_dotfiles.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# Note that is a BARE repo
+git clone --bare git@github.com:fjararibet/.dotfiles.git $HOME/.dotfiles
+
+# we haven't created the dotfiles alias yet,
+# so we create it as a function for now 
+function dotfiles {
+   git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME $@
+}
+if [ $? = 0 ]; then
+  echo "dotfiles successfully installed.";
+  else
+    echo "Moving existing dotfiles to ~/.dotfiles-backup";
+    config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .dotfiles-backup/{}
+fi
+
+dotfiles checkout
+dotfiles config --local status.showUntrackedFiles no
+
