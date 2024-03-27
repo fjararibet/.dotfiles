fe1d48ece74736907d7f5c27eb0a6be46db3a16d
Felipe Jara <felipe.jara@magnet.cl>
2024-03-26 23:52:14 -0300

    update

--- .decrypt_keys.sh
+++ .decrypt_keys.sh
@@ -2,7 +2,7 @@
 
 curl --remote-name https://raw.githubusercontent.com/fjararibet/.dotfiles/main/.encrypted_keys/id_ed25519
 curl --remote-name https://raw.githubusercontent.com/fjararibet/.dotfiles/main/.encrypted_keys/id_ed25519.pub
-cp id_ed25519* $HOME/.ssh
+mv id_ed25519* $HOME/.ssh
 ansible-vault decrypt $HOME/.ssh/id_ed25519*
 
 
