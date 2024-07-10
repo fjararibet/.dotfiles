1a25e86502e4a00238f2bfd313e5159fdc9fb7b0
Felipe Jara <fjararibet@gmail.com>
2024-07-09 22:18:17 -0400

    update

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -9,7 +9,7 @@ return {
   'tpope/vim-sleuth',
 
   -- Useful plugin to show you pending keybinds.
-  {'folke/which-key.nvim', opts = {} },
+  { "folke/which-key.nvim", opts = {} },
 
   -- "gc" to comment visual regions/lines
   { 'numToStr/Comment.nvim', opts = {} },
