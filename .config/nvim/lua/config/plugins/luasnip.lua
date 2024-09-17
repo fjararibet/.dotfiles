a95fbc1f84db4efd4c197bd431bd35ea2632cfb0
Felipe Jara <fjararibet@gmail.com>
2024-09-17 17:19:15 -0300

    update

--- .config/nvim/lua/config/plugins/luasnip.lua
+++ .config/nvim/lua/config/plugins/luasnip.lua
@@ -1,14 +1,23 @@
 local ls = require('luasnip')
 local s = ls.snippet
 local t = ls.text_node
-local i = ls.insert_node
+-- local i = ls.insert_node
 
 ls.add_snippets("cpp", {
   s('comp', {
     t('#include <bits/stdc++.h>'),
     t({ '', 'using namespace std;' }),
-    t({ '', 'int main() {', '' }),
-    t('  '), i(1),
+    t({ '', 'void solve() {' }),
+    t({ '', '}' }),
+    t({ '', 'int main() {' }),
+    t({ '', '  ios::sync_with_stdio(0);' }),
+    t({ '', '  cin.tie(0);' }),
+    t({ '', '  int t;' }),
+    t({ '', '  t = 1;' }),
+    t({ '', '  // cin >> t;' }),
+    t({ '', '  while (t--)' }),
+    t({ '', '    solve();' }),
+    t(''),
     t({ '', '}' }),
   })
 })
