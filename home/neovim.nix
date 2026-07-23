769b0919365ff8f353f4d007e89cb9669d2336ba
Felipe Jara <fjararibet@gmail.com>
2026-07-23 01:34:42 -0400

    make nvim its own module

---
+++ home/neovim.nix
@@ -0,0 +1,55 @@
+{ inputs, lib, pkgs, paths, ... }:
+
+let
+  nvimTreesitter = pkgs.vimPlugins.nvim-treesitter.withAllGrammars;
+  nvimTreesitterWithGrammars = pkgs.symlinkJoin {
+    name = "nvim-treesitter-with-all-grammars";
+    paths = [ nvimTreesitter ] ++ nvimTreesitter.dependencies;
+  };
+
+  nvimPlugins = with pkgs.vimPlugins; {
+    "folke/lazy.nvim" = lazy-nvim;
+    "tpope/vim-sleuth" = vim-sleuth;
+    "folke/which-key.nvim" = which-key-nvim;
+    "Vimjas/vim-python-pep8-indent" = vim-python-pep8-indent;
+    "hrsh7th/nvim-cmp" = nvim-cmp;
+    "L3MON4D3/LuaSnip" = luasnip;
+    "saadparwaiz1/cmp_luasnip" = cmp_luasnip;
+    "hrsh7th/cmp-path" = cmp-path;
+    "hrsh7th/cmp-buffer" = cmp-buffer;
+    "hrsh7th/cmp-nvim-lsp" = cmp-nvim-lsp;
+    "Jezda1337/nvim-html-css" = pkgs.vimUtils.buildVimPlugin {
+      pname = "nvim-html-css";
+      version = "510223bdd5533ed49cad5d8a13ec8b40ab16dcda";
+      src = inputs.nvim-html-css;
+    };
+    "navarasu/onedark.nvim" = onedark-nvim;
+    "rose-pine/neovim" = rose-pine;
+    "rebelot/kanagawa.nvim" = kanagawa-nvim;
+    "stevearc/oil.nvim" = oil-nvim;
+    "nvim-tree/nvim-web-devicons" = nvim-web-devicons;
+    "mbbill/undotree" = undotree;
+    "lewis6991/gitsigns.nvim" = gitsigns-nvim;
+    "nvim-telescope/telescope.nvim" = telescope-nvim;
+    "nvim-lua/plenary.nvim" = plenary-nvim;
+    "nvim-telescope/telescope-fzf-native.nvim" = telescope-fzf-native-nvim;
+    "nvim-lualine/lualine.nvim" = lualine-nvim;
+    "stevearc/conform.nvim" = conform-nvim;
+    "neovim/nvim-lspconfig" = nvim-lspconfig;
+    "nvim-treesitter/nvim-treesitter" = nvimTreesitterWithGrammars;
+    "nvim-treesitter/nvim-treesitter-textobjects" = nvim-treesitter-textobjects;
+  };
+
+  nvimConfig = pkgs.runCommand "nvim-config" { } ''
+    cp -r ${paths.config + "/nvim"} "$out"
+    chmod -R u+w "$out"
+    mkdir -p "$out/lua/config"
+    cat > "$out/lua/config/nix-plugins.lua" <<'EOF'
+    return ${lib.generators.toLua { } (builtins.mapAttrs (_: builtins.toString) nvimPlugins)}
+    EOF
+  '';
+in
+{
+  home.packages = [ pkgs.neovim ];
+  xdg.configFile.nvim.source = nvimConfig;
+}
